a6d0b1089233147a9b04a3c6ba1d872808774ea7
吉井駿一 <syun_syun01@icloud.com>
2021-02-26 02:58:37 +0900

    Chrome以外でアラート表示

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './index.vue';
 
+var userAgent = window.navigator.userAgent.toLowerCase();
+if (userAgent.indexOf('chrome') != -1) {
+  //
+} else {
+  alert('ご覧のブラウザは対応しておりません\nChromeをお使いください');
+}
+
 createApp(App).mount('#app');
