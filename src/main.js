2641d57dedb8fa4f202035c93a6fb8caabccb98c
吉井駿一 <syun_syun01@icloud.com>
2021-02-26 03:05:16 +0900

    build2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ var userAgent = window.navigator.userAgent.toLowerCase();
 if (userAgent.indexOf('chrome') != -1) {
   //
 } else {
-  alert('ご覧のブラウザは対応しておりません\nChromeをお使いください');
+  alert('ご覧のブラウザには対応しておりません\nChromeをお使いください');
 }
 
 createApp(App).mount('#app');
